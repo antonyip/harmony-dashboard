276482163da6e67de6404cc581100e230daf727a
Anton Yip <anton_yip@hotmail.com>
2022-01-28 13:36:36 +0800

    updates

--- src/pages/DefiKingdoms.js
+++ src/pages/DefiKingdoms.js
@@ -28,22 +28,22 @@ function PriceCol(props) {
   props.data.data.forEach(element => {
     if (limit > 0)
     {
-      if (props.type === 'jewel')
+      if (props.type === 'Jewel')
       {
         xAxisData.push(element.JEWEL_PRICE)
         yAxisData.push(element.MDDATE.substr(0,10))
       }
-      if (props.type === 'tear')
+      if (props.type === 'GaiaTear')
       {
         xAxisData.push(element.GAIA_PRICE)
         yAxisData.push(element.MDDATE.substr(0,10))
       }
-      if (props.type === 'shva')
+      if (props.type === 'ShvaRune')
       {
         xAxisData.push(element.RUNE_PRICE)
         yAxisData.push(element.MDDATE.substr(0,10))
       }
-      if (props.type === 'gold')
+      if (props.type === 'Gold')
       {
         xAxisData.push(element.GOLD_PRICE)
         yAxisData.push(element.MDDATE.substr(0,10))
@@ -114,11 +114,225 @@ function PriceWatch() {
 
   return (
     <Row>
-      <PriceCol type='jewel' data={fetchData}></PriceCol>
-      <PriceCol type='tear' data={fetchData}></PriceCol>
-      <PriceCol type='shva' data={fetchData}></PriceCol>
-      <PriceCol type='gold' data={fetchData}></PriceCol>
+      <PriceCol type='Jewel' data={fetchData}></PriceCol>
+      <PriceCol type='GaiaTear' data={fetchData}></PriceCol>
+      <PriceCol type='ShvaRune' data={fetchData}></PriceCol>
+      <PriceCol type='Gold' data={fetchData}></PriceCol>
     </Row>
+  )
+}
+
+function BankPie() {
+
+  const [error, setError] = useState(false);
+  const [fetchData, setFetchData] = useState("");
+  const [unlockedJewelSupply, setUnlockedJewelSupply] = useState(100);
+  const [lockedJewelSupply, setLockedJewelSupply] = useState(100);
+  const [lpJewelSupply, setLpJewelSupply] = useState(100);
+  useEffect( () => {
+
+    // unlockedSupply
+    axios.post("https://api.harmony.one",{
+    "jsonrpc": "2.0",
+    "method": "hmy_call",
+    "params": [
+        {
+            "to": "0x72cb10c6bfa5624dd07ef608027e366bd690048f",
+            "data": "0xfd3d27b8"
+        },
+        "latest"
+    ],
+    "id": 1
+    }).then( 
+      res => {
+      setFetchData(res);
+      setUnlockedJewelSupply(parseInt(res.data.result,16)/10**18)
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  useEffect( () => {
+    // lockedSupply
+    axios.post("https://api.harmony.one",{
+    "jsonrpc": "2.0",
+    "method": "hmy_call",
+    "params": [
+        {
+            "to": "0x72cb10c6bfa5624dd07ef608027e366bd690048f",
+            "data": "0xca5c7b91"
+        },
+        "latest"
+    ],
+    "id": 2
+    }).then( 
+      res => {
+      setFetchData(res);
+      setLockedJewelSupply(parseInt(res.data.result,16)/10**18)
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+  
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_jewel_lp").then( 
+      res => {
+      setLpJewelSupply(res.data[res.data.length-1].TOTAL_LOCKED_JEWEL);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  
+
+  if (error) return <CardBody>Error Loading</CardBody>;
+  if (fetchData === "") return <CardBody>Loading...</CardBody>;
+
+
+  const pieChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Jewel Distribution',
+      },
+    },
+  };
+
+  var jQuestRewards = 2387129;
+  var jDevTeam = 6784755 +  4822886;
+  var jBank = 16851601;
+  var jLP = lpJewelSupply;
+  var jLocked = lockedJewelSupply
+  var jWallet = unlockedJewelSupply - jDevTeam - jBank - jLP - jQuestRewards
+  const pieData = {
+    labels: ['DevTeam', 'Bank', 'LP', 'Players', 'QuestRewards', 'Locked'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [jDevTeam, jBank, jLP, jWallet, jQuestRewards, jLocked],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Pie data={pieData} options={pieChartOptions}/>
+  )
+}
+
+function BankPie2() {
+  const [error, setError] = useState(false);
+  const [fetchData, setFetchData] = useState("");
+  const [unlockedJewelSupply, setUnlockedJewelSupply] = useState(100)
+  const [lpJewelSupply,setLpJewelSupply] = useState(100)
+  useEffect( () => {
+
+    // unlockedSupply
+    axios.post("https://api.harmony.one",{
+    "jsonrpc": "2.0",
+    "method": "hmy_call",
+    "params": [
+        {
+            "to": "0x72cb10c6bfa5624dd07ef608027e366bd690048f",
+            "data": "0xfd3d27b8"
+        },
+        "latest"
+    ],
+    "id": 1
+    }).then( 
+      res => {
+      setFetchData(res);
+      setUnlockedJewelSupply(parseInt(res.data.result,16)/10**18)
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_jewel_lp").then( 
+      res => {
+      setLpJewelSupply(res.data[res.data.length-1].TOTAL_LOCKED_JEWEL);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  if (error) return <CardBody>Error Loading</CardBody>;
+  if (fetchData === "") return <CardBody>Loading...</CardBody>;
+
+  const pieChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Unlocked Jewel Distribution',
+      },
+    },
+  };
+
+  var jQuestRewards = 2387129;
+  var jDevTeam = 6784755 + 4822886;
+  var jBank = 16851601;
+  var jLP = lpJewelSupply;
+  var jWallet = unlockedJewelSupply - jDevTeam - jBank - jLP - jQuestRewards
+
+  const pieData = {
+    labels: ['DevTeam','Bank', 'LP', 'Players', 'QuestRewards'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [jDevTeam, jBank, jLP, jWallet, jQuestRewards],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <Pie data={pieData} options={pieChartOptions}/>
   )
 }
 
@@ -217,110 +431,363 @@ function BankWatch() {
     ],
   };
 
-  const pieChartOptions = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Jewel Distribution',
-      },
-    },
-  };
-
-  const pieData = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const pieChartOptions2 = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'xJewel Distribution',
-      },
-    },
-  };
-
-  const pieData2 = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
 /*
 // balance = jewels, total supply = xjewels
 {"ratio":1.6543417801800981,"balance":15549588.234933916,"timestamp":1641686524792,"totalSupply":9399259.82721728}
 */
   return (
     <Row>
+      <Col xs='2'><BankPie/></Col>
       <Col xs='4'><Line options={chartOptions} data={chartDataTotalJewel} /></Col>
       <Col xs='4'><Line options={chartOptions} data={chartDataRatio} /></Col>
-      <Col xs='2'><Pie options={pieChartOptions} data={pieData} /></Col>
-      <Col xs='2'><Pie options={pieChartOptions2} data={pieData2} /></Col>
+      <Col xs='2'><BankPie2/></Col>
     </Row>
   );
 }
 
 function PoolWatch() {
-  return <Col>pool watch..</Col>
-}
+  const [error, setError] = useState(false);
+  const [fetchData, setFetchData] = useState("");
 
-function QuestWatch() {
-  return <Col>quest watch..</Col>
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_new_profiles").then( 
+      res => {
+      setFetchData(res);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  if (error) return <CardBody>Error Loading...</CardBody>;
+  if (fetchData === "") return <CardBody>Loading...</CardBody>;
+
+  var limit = 7;
+  var xAxisData = []
+  var yAxisData= []
+
+  fetchData.data.forEach( element => {
+    if (limit > 0)
+    {
+      xAxisData.push(element.DAY_DATE.substr(0,10))
+      yAxisData.push(element.DAILY_NEW_PROFILES);
+      limit -= 1;
+    }
+  })
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: 'Daily New Profiles',
+      },
+    },
+  };
+
+  const chartData = {
+    labels: xAxisData,
+    datasets: [
+      {
+        label: "Daily New Profiles",
+        data: yAxisData,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
+
+  
+  return (
+    <Row>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+    </Row>
+  )
 }
 
 function HeroWatch() {
-  return <Col>hero watch..</Col>
+  const [error, setError] = useState(false);
+  const [fetchData, setFetchData] = useState("");
+
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_new_profiles").then( 
+      res => {
+      setFetchData(res);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  if (error) return <CardBody>Error Loading...</CardBody>;
+  if (fetchData === "") return <CardBody>Loading...</CardBody>;
+
+  var limit = 7;
+  var xAxisData = []
+  var yAxisData= []
+
+  fetchData.data.forEach( element => {
+    if (limit > 0)
+    {
+      xAxisData.push(element.DAY_DATE.substr(0,10))
+      yAxisData.push(element.DAILY_NEW_PROFILES);
+      limit -= 1;
+    }
+  })
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: 'Daily New Profiles',
+      },
+    },
+  };
+
+  const chartData = {
+    labels: xAxisData,
+    datasets: [
+      {
+        label: "Daily New Profiles",
+        data: yAxisData,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
+
+  
+  return (
+    <Row>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+    </Row>
+  )
+}
+
+function QuestWatch() {
+  const [error, setError] = useState(false);
+  const [fetchData, setFetchData] = useState("");
+
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_new_profiles").then( 
+      res => {
+      setFetchData(res);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  if (error) return <CardBody>Error Loading...</CardBody>;
+  if (fetchData === "") return <CardBody>Loading...</CardBody>;
+
+  var limit = 7;
+  var xAxisData = []
+  var yAxisData= []
+
+  fetchData.data.forEach( element => {
+    if (limit > 0)
+    {
+      xAxisData.push(element.DAY_DATE.substr(0,10))
+      yAxisData.push(element.DAILY_NEW_PROFILES);
+      limit -= 1;
+    }
+  })
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: 'Daily New Profiles',
+      },
+    },
+  };
+
+  const chartData = {
+    labels: xAxisData,
+    datasets: [
+      {
+        label: "Daily New Profiles",
+        data: yAxisData,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
+
+  
+  return (
+    <Row>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+    </Row>
+  )
+}
+
+function GameWatch() {
+  const [error, setError] = useState(false);
+  const [fetchData, setFetchData] = useState("");
+  const [fetchDataHero, setFetchDataHero] = useState("");
+
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_new_profiles").then( 
+      res => {
+      setFetchData(res);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  useEffect( () => {
+    axios.get("https://dfkreport.antonyip.com/dfk-backend/?q=daily_hero_summons").then( 
+      res => {
+        setFetchDataHero(res);
+    }).catch( err => {
+      console.log(err)
+      setError(true);
+    })
+  } ,[]);
+
+  if (error) return <CardBody>Error Loading...</CardBody>;
+  if (fetchData === "") return <CardBody>Loading...</CardBody>;
+  if (fetchDataHero === "") return <CardBody>Loading...</CardBody>;
+  
+  var limit = 7;
+  var xAxisData = []
+  var yAxisData= []
+
+  fetchData.data.forEach( element => {
+    if (limit > 0)
+    {
+      xAxisData.push(element.DAY_DATE.substr(0,10))
+      yAxisData.push(element.DAILY_NEW_PROFILES);
+      limit -= 1;
+    }
+  })
+
+  var xAxisData2 = []
+  var yAxisData2= []
+  limit = 7
+  fetchDataHero.data.forEach( element => {
+    if (limit > 0)
+    {
+      xAxisData2.push(element.DAY_DATE.substr(0,10))
+      yAxisData2.push(element.HERO_SUMMONS);
+      limit -= 1;
+    }
+  })
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: 'Daily New Profiles',
+      },
+    },
+  };
+
+  const chartData = {
+    labels: xAxisData,
+    datasets: [
+      {
+        label: "Daily New Profiles",
+        data: yAxisData,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
+
+  const chartOptions2 = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: 'Daily Hero Summon Count',
+      },
+    },
+  };
+
+  const chartData2 = {
+    labels: xAxisData2,
+    datasets: [
+      {
+        label: "Daily Hero Summon Count",
+        data: yAxisData2,
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
+
+  
+  return (
+    <Row>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+      <Col xs='4'><Line data={chartData2} options={chartOptions2}></Line></Col>
+      <Col xs='4'><Line data={chartData} options={chartOptions}></Line></Col>
+    </Row>
+  )
 }
 
 
@@ -341,6 +808,7 @@ function DefiKingdoms() {
       </Card>
       <Card>
         <CardHeader>DefiKingdoms - Game Stats</CardHeader>
+        <GameWatch></GameWatch>
         <QuestWatch></QuestWatch>
         <HeroWatch></HeroWatch>
       </Card>
@@ -349,3 +817,4 @@ function DefiKingdoms() {
 }
 
 export default DefiKingdoms;
+
